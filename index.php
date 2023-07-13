c9ec1b99a7c911e9948e28276a976f3f635fae58
randji <stephanesoilihi@yahoo.fr>
2023-07-13 22:15:00 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
+    <title>Garage V.Garrot</title>
+</head>
+<body>
+    <header>
+        <div class="titre_entreprise">Garage V.Garrot</div>
+        <div class="navbar">
+            <ul class="liste">
+                <li class="element">Réparation carrosserie</li>
+                <li class="element"> Mécanique des voitures</li>
+                <li class="element">Entretien régulier</li>
+                <li class="element">Vente de véhicule d'occasion</li>
+            </ul>
+        </div>
+    </header>
+    <main class="main">
+        <div class="logo">
+            <img src="img/logogarage.png" alt="logo garage" width="568px" height="255px">
+        </div>
+        <div class="phrase">
+            <h2>"Laissez votre voiture <br> entre de bonnes mains"</h2>
+        </div>
+    </main>
+    <footer>
+        <a href="Tel:0112547585" class="telephone">tél : 01 12 54 75 85</a>
+        <div class="horaire">
+            <ul class="list_horaire">
+                <li class="element_horaire">HORAIRE</li>
+                <li class="element_horaire">lundi au samedi : 9h-18h</li>
+                <li class="element_horaire">dimanche : Fermé</li>
+            </ul>
+        </div>
+        <form action="" method="post" class="formulaire">
+            <button type="submit">formulaire contact</button>
+        </form>
+    </footer>
+    
+</body>
+</html>
